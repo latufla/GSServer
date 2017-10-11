8c963cfa783923a2d9f6b0a03f3996a78c163e1e
latufla <lafufla.work@gmail.com>
2017-10-11 15:00:13 +0700

    Remove comments

--- event/Get_Size.js
+++ event/Get_Size.js
@@ -1,10 +1,3 @@
-// ====================================================================================================
-//
-// Cloud Code for Get_Size, write your code here to customize the GameSparks platform.
-//
-// For details of the GameSparks Cloud Code API see https://docs.gamesparks.com/
-//
-// ====================================================================================================
 var player = Spark.getPlayer()
 var size_v = player.getScriptData("SIZEVAR");
 Spark.setScriptData("SIZE", size_v);
--- event/Set_Size.js
+++ event/Set_Size.js
@@ -1,11 +1,3 @@
-// ====================================================================================================
-//
-// Cloud Code for Set_Size, write your code here to customize the GameSparks platform.
-//
-// For details of the GameSparks Cloud Code API see https://docs.gamesparks.com/
-//
-// ====================================================================================================
-
 var size_v = Spark.getData().SIZE;
 var player = Spark.getPlayer();
 player.setScriptData("SIZEVAR", size_v);
